c6227802e731f446e8d6e6d4a5f333703a2577e2
chebihiFEB <faycal.chedihi@ext-energy-pool.eu>
2024-07-20 13:23:42 +0200

    add NewExpense

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:exprense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:exprense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 
 import '../models/expense.dart';
@@ -26,9 +27,20 @@ class _ExpensesState extends State<Expenses> {
         category: Category.leisure)
   ];
 
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(context: context, builder: (ctx) => NewExpense());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter ExpenseTracker'),
+        actions: [
+          IconButton(
+              onPressed: _openAddExpenseOverlay, icon: const Icon(Icons.add))
+        ],
+      ),
       body: Column(
         children: [
           const Text("The chart"),
